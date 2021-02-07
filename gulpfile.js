4f9de723a88eef295ccc95f1e82d728a2354d2b4
Audrey Diez <audrey.diez@gmail.com>
2021-02-07 15:39:06 +0100

    Feature: autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,16 @@ const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 function scssTask(){
     return src('sass/main.scss', { sourcemaps: true })
         .pipe(sass())
         .pipe(postcss([cssnano()]))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(dest('public/css', { sourcemaps: '.' }));
 }
 
